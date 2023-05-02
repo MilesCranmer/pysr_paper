e15a2a3d248c15c3a17d92198e505ae446f828f3
MilesCranmer <miles.cranmer@gmail.com>
2023-05-02 11:30:16 -0400

    Fix minted path not showing up

--- src/scripts/preprocess_arxiv_tex.sh
+++ src/scripts/preprocess_arxiv_tex.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pushd $1
-rm **/*.bib
 sed -i ms.tex -e 's/finalizecache/frozencache/g'
 sed -i ms.tex -e '/PassOptionsToPackage/d'
 sed -i showyourwork.tex -e 's/RequirePackage{xcolor}/RequirePackage[table]{xcolor}/g'
